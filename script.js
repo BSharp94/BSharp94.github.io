f0b3bf9157c56cfb96fb165eb17a651e9712ca4c
Brian Sharp <briansharp1994@gmail.com>
2017-12-13 16:05:07 -0500

    Add basic skeleton

---
+++ script.js
@@ -0,0 +1,11 @@
+window.onload = () => {
+
+  var vm = new Vue({
+    el: '#app',
+    data: {
+      languages: ['C', 'Java', 'Javascript', 'Python'],
+      categories: ['Hiring', 'General Knowledge'] 
+    }
+  })
+
+}
