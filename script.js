7f16bb4fdc2be1321962ca7d484fc1977fd1f83e
Brian Sharp <bsharp@brians-mbp.attlocal.net>
2018-02-15 22:14:03 -0500

    Big push. New UI

--- script.js
+++ script.js
@@ -4,8 +4,22 @@ window.onload = () => {
     el: '#app',
     data: {
       languages: ['C', 'Java', 'Javascript', 'Python'],
-      categories: ['Hiring', 'General Knowledge'] 
+      categories: ['Hiring', 'General Knowledge'],
+      aboutMeData: "tesst this is a text",
     }
-  })
+  });
 
+  var aboutMeData = "test test test".split(" ").reverse();
+
+  // window.setInterval(
+  //   () => {
+      
+  //     var newText = $("#aboutMeText").text() + aboutMeData.pop();
+  //     $("#aboutMeText").text(newText)
+
+  //     // clear interval
+  //     if(aboutMeData.length = 0|| aboutMeData == null)
+  //       clearInterval
+  //   }
+  // ,500);
 }
