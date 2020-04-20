e44add444c3cdaf760f94e389899836b3fcbe804
Brian Sharp <briansharp1994@gmail.com>
2020-04-20 05:38:01 -0400

    Update styling

--- profile-page/src/components/services/services.js
+++ profile-page/src/components/services/services.js
@@ -29,9 +29,18 @@ class Services extends React.Component {
 
         return (
             <div className = "services__panel row">
-                <ServicesCard title = "Web Development" summary = {summ}  color = "blue" />
-                <ServicesCard title = "Data Visualization" summary = {summ}  color = "green" />
-                <ServicesCard title = "Machine Learning" summary = {summ}  color = "purple" />
+                <ServicesCard 
+                    title = "Web Development" 
+                    summary = {"I develop full stack solutions for data driven web applications. My services range from front-end UI/UX development to designing transaction and analytical databases."}  
+                    color = "blue" />
+                <ServicesCard 
+                    title = "Data Visualization" 
+                    summary = {"I help to produce stunning data visualizations that provide useful insights to users. I use modern frontend framworks to develop fast and responsive dashboards."}  
+                    color = "green" />
+                <ServicesCard 
+                    title = "Machine Learning" 
+                    summary = {"I create cutting edge machine learning models. Specialties include computer vision, natural language processing, and predictive analytics."}  
+                    color = "purple" />
             </div>
         )
     }
